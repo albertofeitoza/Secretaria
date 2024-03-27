1a557631435f6e379c3cdded7c8cf0bd48b3bbe5
Alberto <alberto.feitoza@outlook.com>
2024-03-27 02:19:37 -0300

    correção de enviroment

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     environmentName : "dev",
-    BASE_URL : 'http://localhost:5000/api'
+    BASE_URL : 'http://192.168.0.108:5000/api'
     //BASE_URL : '/api'
 
   };
