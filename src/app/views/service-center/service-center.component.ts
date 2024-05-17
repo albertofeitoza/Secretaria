6f8d05edfb57796a4da153bb4256fd8c99601367
Alberto <alberto.feitoza@outlook.com>
2024-05-17 02:30:17 -0300

    AJuste para pegar cartão vencido

--- src/app/views/service-center/service-center.component.ts
+++ src/app/views/service-center/service-center.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Injectable, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource, _MatTableDataSource } from '@angular/material/table';
 import { Endpoint } from 'src/app/enum/Endpoints';
 import { ServiceCenter } from 'src/app/models/ServiceCenter';
 import { AllservicesService } from 'src/app/services/allservices.service';
 import { AutenticacaoService } from 'src/app/services/autenticacao.service';
+
+@Injectable()
 
 @Component({
   selector: 'app-service-center',
@@ -14,19 +16,19 @@ import { AutenticacaoService } from 'src/app/services/autenticacao.service';
 })
 export class ServiceCenterComponent implements OnInit {
 
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  datasource = new MatTableDataSource<any>();
-
   tipoUsuario: Number = 0;
   Colunas = ['id', 'nomeMembro', 'departamento', 'nomePendencia'];
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  datasource = new MatTableDataSource<ServiceCenter>();
 
   constructor(
     private auth: AutenticacaoService,
     private serviceApi: AllservicesService<any>
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.tipoUsuario = this.UsuarioLogado();
     this.BuscarPendencias();
   }
@@ -40,9 +42,10 @@ export class ServiceCenterComponent implements OnInit {
     return this.auth.tipoUsuarioLogado
   }
 
-  public BuscarPendencias(): void {
+  BuscarPendencias(){
     this.serviceApi.read(Endpoint.ServiceCenter)
-      .subscribe((result: ServiceCenter[]) => {
+      .subscribe((result : ServiceCenter[]) => {
+        result = result.sort()
         this.datasource.data = result
         this.datasource.sort = this.sort
       });
